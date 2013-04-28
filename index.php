010c2da25a3e083405bc2aca219e25151f3c7d10
manojmediaindia <manojmediaindia@gmail.com>
2013-04-28 23:34:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<title>Nature Wave</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Nature Wave, le spécialiste du conseil et des solutions relatives aux pollutions électromagnétiques" />
+<meta name="Keywords" content="Nature Wave, Wifi Protection, Electro Waves, Protection" />
+<meta name="robots" content="noindex, nofollow">
+<link rel="icon" href="images/favicon.gif" sizes="32x32">
+<link rel="shortcut icon" href="images/favicon.gif" type="image/x-icon">
+
+<link href="Style/style.css" rel="stylesheet" type="text/css" media="screen, projection" />
+
+</head>
+
+<body>
+<?php include("header.php"); ?>
+
+<div class="banner">
+<img src="Images/Banner_1.png"  alt="banner" />
+</div>
+
+    <div class="index_container">
+    
+        
+        <div class="midsection">
+<div class="row-fluid">
+<section>
+<ul>
+<li class="span1">
+<div class="wifi"></div><a href="#" title="Wifi"></a>
+</li>
+<li class="span1">
+<div class="cellantenna"></div><span><a href="#" title="Cell Antenna"></a></span>
+</li>
+<li class="span1">
+<div class="bluetooth"></div><span><a href="#" title="Bluetooth"></a></span>
+</li>
+<li class="span1">
+<div class="cordlessphone"></div><span><a href="#" title="Phone"></a></span>
+</li>
+<li class="span1">
+<div class="tablet"></div><span><a href="#" title="Tablet"></a></span>
+</li>
+<li class="span1">
+<div class="laptop"></div><span><a href="#" title="Laptop"></a></span>
+</li>
+<li class="span1">
+<div class="powerline"></div><span><a href="#" title="Power Line"></a></span>
+</li>
+<li class="span1">
+<div class="babyphone"></div><span><a href="#" title="Baby Phone"></a></span>
+</li>
+<li class="span1">
+<div class="electricmeter"></div><span><a href="#" title="Electric Meter"></a></span>
+</li>
+<li class="span1">
+<div class="homeprotection"></div><span><a href="#" title="Home Protection"></a></span>
+</li>
+<li class="span1">
+<div class="smartphone"></div><span><a href="#" title="Smart Phone"></a></span>
+</li>
+<li class="span1">
+<div class="bodyprotection"></div><span><a href="#" title="Body Protection"></a></span>
+</li>
+</ul>
+</section>
+</div>
+    
+    
+    
+    </div>
+
+
+        <div class="info_container">
+
+        <h1 class="info">Information</h1>
+          <div class="info_box">
+          
+              <div class="box1">
+              <img src="Images/Protection de habitat.jpg" width="265px;" alt="NW" />
+                  <h2 class="info-head">Protection de l'habitat</h2>
+                  <p class="info-content">Le domicile, ou le lieu de travail sont g&#233;n&#233;ralement charg&#233;s en pollutions &#233;lectromagn&#233;tiques. La diffusion permanente du Wifi, la proximit&#233; d'antennes-relais ou de lignes hautes tensions...</p>
+                  <div class="more_info">
+                  <a class="info_more" href="">Lire</a>
+                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                      </div>
+              </div>
+
+              <div class="box2">
+              <img src="Images/dangers-wifi.jpg" width="265px;" alt="NW" />
+                  <h2 class="info-head">Les dangers du Wifi</h2>
+                  <p class="info-content">La grande majorit&#233; des foyers sont expos&#233;s &#224; un ou plusieurs r&#233;seaux WiFi. Le signal Wi-fi d&#233;sorganise les processus physiologiques et d&#233;truit des structures biochimiques...</p>
+                  <div class="more_info">
+                  <a class="info_more" href="">Lire</a>
+                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                      </div>
+              </div>
+
+              <div class="box3">
+              <img src="Images/telephone-mobile.jpg" width="265 px;" alt="NW" />
+                  <h2 class="info-head">Le t&#233;l&#233;phone Mobile</h2>
+                  <p class="info-content">Depuis de nombreuses ann&#233;es des &#233;tudes tendent &#224; d&#233;montrer les risques pour la sant&#233; de l'usage du t&#233;l&#233;phone portable. L'Organisation Mondiale de la Sant&#233; (OMS) a indiqu&#233;...</p>
+                  <div class="more_info">
+                  <a class="info_more" href="">Lire</a>
+                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                      </div>
+              </div>
+
+          </div>
+        </div>
+
+
+        <div class="edu_adv">
+        <div class="edu">
+        <h2 class="edu_adv_heading">Etudes et Informations</h2>
+
+            <div class="edu_adv_box">
+            <p class="edu_text">
+            On retrouve beaucoup d'information et d'&#233;tudes, parfois contradictoires au sujet du risque portant sur le sujet des &#233;tudes &#233;lectromagn&#233;tiques. Ce site a pour vocation de recenser pour les diff&#233;  rentes technologies de diffusion et d'&#233;missions d'ondes, les risques reconnus selon les &#233;tudes publi&#233;es.
+            </p>
+                <div class="more_info1">
+                  <a class="info_more1" href="">Lire</a>
+                  <img class="info_more_img1" src="images/read_more.png" width="10px" alt="Read More" />
+                      </div>
+            </div>
+
+        </div>
+            <div class="adv">
+            <h2 class="edu_adv_heading">Conseil et Solutions</h2>
+
+                <div class="edu_adv_box">
+            <p class="edu_text">On trouve de nombreuses r&#232;gles de bon sens qui permettent de limiter l'exposition et par cons&#233;quent l'impact des ondes sur les personnes.
+
+Dans le cas du t&#233;l&#233;phone portable, nous pr&#233;conisons les mesures suivantes propos&#233;es par l'appel de 20 experts internationaux rassembl&#233;s par David Servan-Schreiber en 2008 <a href="">Link to PDF</a> Cet appel, fait echo &#224; l'appel des medecins de Fribourg lanc&#233; d&#232;s 2002. <a href="">Link to PDF</a> </p>
+                    <div class="more_info1">
+                  <a class="info_more1" href="">Lire</a>
+                  <img class="info_more_img1" src="images/read_more.png" width="10px" alt="Read More" />
+                      </div>
+            </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+     <?php include("footer.php"); ?>   
+</body>
+</html>
