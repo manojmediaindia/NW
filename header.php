ad71391e367ad84f9cd226294cbc5438c2575ac2
manojmediaindia <manojmediaindia@gmail.com>
2013-04-28 23:36:25 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<div class="menu_main">
+<div class="menu_icons">
+<a href=""><img alt="Follow on Facebook" src="Images/fb_icon.png" /></a>
+<a href=""><img alt="Twitter" src="Images/twitter_icon.gif" /></a>
+<a href=""><img alt="Blog" src="Images/fb_icon.png" /></a>
+<a href=""><img alt="YouTube" src="Images/twitter_icon.gif" /></a>
+</div>
+<div class="menu_quote"><span>"Nature Wave, le spécialiste du conseil et des solutions relatives aux pollutions électromagnétiques"</span></div>
+</div>
+<div class="clear"></div>
+
+<div class="header">
+<div class="logo">
+<a href="index.php"><img src="Images/Logo.gif" alt="Logo - Nature Wave" /></a>
+</div>
+<div class="search">
+<input type="text" value="Search.." class="input_search" onkeypress="return kd(event);" onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'Search...':this.value;" />
+<input type="button" value="Go!" class="input_button" />
+<a href=""><img class="cart_img" src="Images/Shopping-cart.gif" alt="View Cart" /></a>
+</div>
+    <div class="nav">
+<ul>
+<li><a href="">Présentation</a></li>
+    <li><a href="">Etudes et informations</a></li>
+        <li><a href="">Conseils et solutions</a></li>
+            <li><a href="">Contacts</a></li>
+</ul>
+</div>
+</div>
+
