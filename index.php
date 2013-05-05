55cb4a99e6d565517e9047931470a94e8b0d6247
frej <github@oliana.net>
2013-05-05 09:59:51 +0200

    - fixes some images link

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 <meta name="robots" content="noindex, nofollow">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
 
-<link rel="icon" href="images/favicon.gif" sizes="32x32">
-<link rel="shortcut icon" href="images/favicon.gif" type="image/x-icon">
+<link rel="icon" href="Images/favicon.gif" sizes="32x32">
+<link rel="shortcut icon" href="Images/favicon.gif" type="image/x-icon">
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
  
@@ -87,7 +87,7 @@
                   <p class="info-content">Le domicile, ou le lieu de travail sont g&#233;n&#233;ralement charg&#233;s en pollutions &#233;lectromagn&#233;tiques. La diffusion permanente du Wifi, la proximit&#233; d'antennes-relais ou de lignes hautes tensions...</p>
                   <div class="more_info">
                   <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
                       </div>
               </div>
 
@@ -97,7 +97,7 @@
                   <p class="info-content">La grande majorit&#233; des foyers sont expos&#233;s &#224; un ou plusieurs r&#233;seaux WiFi. Le signal Wi-fi d&#233;sorganise les processus physiologiques et d&#233;truit des structures biochimiques...</p>
                   <div class="more_info">
                   <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
                       </div>
               </div>
 
@@ -107,7 +107,7 @@
                   <p class="info-content">Depuis de nombreuses ann&#233;es des &#233;tudes tendent &#224; d&#233;montrer les risques pour la sant&#233; de l'usage du t&#233;l&#233;phone portable. L'Organisation Mondiale de la Sant&#233; (OMS) a indiqu&#233;...</p>
                   <div class="more_info">
                   <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="images/read_more.png" width="10px" alt="Read More" />
+                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
                       </div>
               </div>
 
@@ -123,7 +123,7 @@
             <p class="edu_text">On retrouve beaucoup d'information et d'&#233;tudes, parfois contradictoires au sujet du risque portant sur le sujet des &#233;tudes &#233;lectromagn&#233;tiques. Ce site a pour vocation de recenser pour les diff&#233;	rentes technologies de diffusion et d'&#233;missions d'ondes, les risques reconnus selon les &#233;tudes publi&#233;es.</p>
                 <div class="more_info1">
                   <a class="info_more1" href="">Lire</a>
-                  <img class="info_more_img1" src="images/read_more.png" width="10px" alt="Read More" />
+                  <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
                       </div>
             </div>
 
@@ -135,7 +135,7 @@
             <p class="edu_text">On trouve de nombreuses r&#232;gles de bon sens qui permettent de limiter l'exposition et par cons&#233;quent l'impact des ondes sur les personnes. Dans le cas du t&#233;l&#233;phone portable, nous pr&#233;conisons les mesures suivantes propos&#233;es par l'appel de 20 experts internationaux rassembl&#233;s par David Servan-Schreiber en 2008 <a class="link-to-pdf" href="">Link to PDF</a> Cet appel, fait echo &#224; l'appel des medecins de Fribourg lanc&#233; d&#232;s 2002. <a class="link-to-pdf" href="">Link to PDF</a> </p>
                     <div class="more_info1">
                   <a class="info_more1" href="">Lire</a>
-                  <img class="info_more_img1" src="images/read_more.png" width="10px" alt="Read More" />
+                  <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
                       </div>
             </div>
 
