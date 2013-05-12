3e9cf4bf4c8cea3e36bb71a32ae9d570340dbf73
frej <github@oliana.net>
2013-05-12 05:36:28 +0200

    - update to the latest google docs

--- index.php
+++ index.php
@@ -1,110 +1,104 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 
-<head>
+    <head>
+        <title>Nature Wave</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="description" content="Nature Wave, le spécialiste du conseil et des solutions relatives aux pollutions électromagnétiques" />
+        <meta name="Keywords" content="Nature Wave, Wifi Protection, Electro Waves, Protection" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+        <link rel="icon" href="Images/favicon.gif" sizes="32x32" />
+        <link rel="shortcut icon" href="Images/favicon.gif" type="image/x-icon" />
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+        </script>
+        <link href="Style/Style.css" rel="stylesheet" type="text/css" media="screen, projection" />
+    </head>
 
-<title>Nature Wave</title>
+    <body>
+        <?php include("header.php"); ?>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="description" content="Nature Wave, le spécialiste du conseil et des solutions relatives aux pollutions électromagnétiques" />
-<meta name="Keywords" content="Nature Wave, Wifi Protection, Electro Waves, Protection" />
-<meta name="robots" content="noindex, nofollow">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
-
-<link rel="icon" href="Images/favicon.gif" sizes="32x32">
-<link rel="shortcut icon" href="Images/favicon.gif" type="image/x-icon">
-
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
- 
-
-<link href="Style/Style.css" rel="stylesheet" type="text/css" media="screen, projection" />
-
-</head>
-
-<body>
-<?php include("header.php"); ?>
-
-<div class="banner">
-<img src="Images/banner_new.gif"  alt="banner" />
-</div>
-
-    <div class="index_container">
-    
-              <?php include("icons.php"); ?>
-
-        <div class="info_container">
-
-        <h1 class="info">Information</h1>
-          <div class="info_box">
-          
-              <div class="box1">
-              <img class="boximg" src="Images/Protection-de-habitat.gif" width="265px;" alt="NW" />
-                  <h2 class="info-head">Protection de l'habitat</h2>
-                  <p class="info-content">Le domicile, ou le lieu de travail sont g&#233;n&#233;ralement charg&#233;s en pollutions &#233;lectromagn&#233;tiques. La diffusion permanente du Wifi, la proximit&#233; d'antennes-relais ou de lignes hautes tensions...</p>
-                  <div class="more_info">
-                  <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
-                      </div>
-              </div>
-
-              <div class="box2">
-              <img class="boximg"  src="Images/dangers-wifi.gif" width="265px;" alt="NW" />
-                  <h2 class="info-head">Les dangers du Wifi</h2>
-                  <p class="info-content">La grande majorit&#233; des foyers sont expos&#233;s &#224; un ou plusieurs r&#233;seaux WiFi. Le signal Wi-fi d&#233;sorganise les processus physiologiques et d&#233;truit des structures biochimiques...</p>
-                  <div class="more_info">
-                  <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
-                      </div>
-              </div>
-
-              <div class="box3">
-              <img class="boximg"  src="Images/telephone-mobile.gif" width="265 px;" alt="NW" />
-                  <h2 class="info-head">Le t&#233;l&#233;phone Mobile</h2>
-                  <p class="info-content">Depuis de nombreuses ann&#233;es des &#233;tudes tendent &#224; d&#233;montrer les risques pour la sant&#233; de l'usage du t&#233;l&#233;phone portable. L'Organisation Mondiale de la Sant&#233; (OMS) a indiqu&#233;...</p>
-                  <div class="more_info">
-                  <a class="info_more" href="">Lire</a>
-                  <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
-                      </div>
-              </div>
-
-          </div>
+        <div class="banner">
+            <img src="Images/banner_new.gif"  alt="banner" />
         </div>
 
+        <div class="index_container">
 
-        <div class="edu_adv">
-        <div class="edu">
-        <h2 class="edu_adv_heading">Etudes et Informations</h2>
+            <?php include("icons.php"); ?>
 
-            <div class="edu_adv_box">
-            <p class="edu_text">On retrouve beaucoup d'information et d'&#233;tudes, parfois contradictoires au sujet du risque portant sur le sujet des &#233;tudes &#233;lectromagn&#233;tiques. Ce site a pour vocation de recenser pour les diff&#233;	rentes technologies de diffusion et d'&#233;missions d'ondes, les risques reconnus selon les &#233;tudes publi&#233;es.</p>
-                <div class="more_info1">
-                  <a class="info_more1" href="">Lire</a>
-                  <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
-                      </div>
+            <div class="info_container">
+
+                <h1 class="info">Information</h1>
+                <div class="info_box">
+
+                    <div class="box1">
+                        <img class="boximg" src="Images/Protection-de-habitat.gif" width="265px;" alt="NW" />
+                        <h2 class="info-head">Protection de l'habitat</h2>
+                        <p class="info-content">Le domicile, ou le lieu de travail sont g&#233;n&#233;ralement charg&#233;s en pollutions &#233;lectromagn&#233;tiques. La diffusion permanente du Wifi, la proximit&#233; d'antennes-relais ou de lignes hautes tensions...</p>
+                        <div class="more_info">
+                            <a class="info_more" href="">Lire</a>
+                            <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
+                        </div>
+                    </div>
+
+                    <div class="box2">
+                        <img class="boximg"  src="Images/dangers-wifi.gif" width="265px;" alt="NW" />
+                        <h2 class="info-head">Les dangers du Wifi</h2>
+                        <p class="info-content">La grande majorit&#233; des foyers sont expos&#233;s &#224; un ou plusieurs r&#233;seaux WiFi. Le signal Wi-fi d&#233;sorganise les processus physiologiques et d&#233;truit des structures biochimiques...</p>
+                        <div class="more_info">
+                            <a class="info_more" href="">Lire</a>
+                            <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
+                        </div>
+                    </div>
+
+                    <div class="box3">
+                        <img class="boximg"  src="Images/telephone-mobile.gif" width="265 px;" alt="NW" />
+                        <h2 class="info-head">Le t&#233;l&#233;phone Mobile</h2>
+                        <p class="info-content">Depuis de nombreuses ann&#233;es des &#233;tudes tendent &#224; d&#233;montrer les risques pour la sant&#233; de l'usage du t&#233;l&#233;phone portable. L'Organisation Mondiale de la Sant&#233; (OMS) a indiqu&#233;...</p>
+                        <div class="more_info">
+                            <a class="info_more" href="">Lire</a>
+                            <img class="info_more_img" src="Images/read_more.png" width="10px" alt="Read More" />
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
-        </div>
-        <div class="adv">
-            <h2 class="edu_adv_heading">Conseil et Solutions</h2>
 
-                <div class="edu_adv_box">
-            <p class="edu_text">On trouve de nombreuses r&#232;gles de bon sens qui permettent de limiter l'exposition et par cons&#233;quent l'impact des ondes sur les personnes. Dans le cas du t&#233;l&#233;phone portable, nous pr&#233;conisons les mesures suivantes propos&#233;es par l'appel de 20 experts internationaux rassembl&#233;s par David Servan-Schreiber en 2008 <a class="link-to-pdf" href="">Link to PDF</a> Cet appel, fait echo &#224; l'appel des medecins de Fribourg lanc&#233; d&#232;s 2002. <a class="link-to-pdf" href="">Link to PDF</a> </p>
-                    <div class="more_info1">
-                  <a class="info_more1" href="">Lire</a>
-                  <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
-                      </div>
-            </div>
+            <div class="edu_adv">
+                <div class="edu">
+                    <h2 class="edu_adv_heading">Etudes et Informations</h2>
+
+                    <div class="edu_adv_box">
+                        <p class="edu_text">On retrouve beaucoup d'information et d'&#233;tudes, parfois contradictoires au sujet du risque portant sur le sujet des &#233;tudes &#233;lectromagn&#233;tiques. Ce site a pour vocation de recenser pour les diff&#233;	rentes technologies de diffusion et d'&#233;missions d'ondes, les risques reconnus selon les &#233;tudes publi&#233;es.</p>
+                        <div class="more_info1">
+                            <a class="info_more1" href="">Lire</a>
+                            <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
+                        </div>
+                    </div>
+
+                </div>
+                <div class="adv">
+                    <h2 class="edu_adv_heading">Conseil et Solutions</h2>
+
+                    <div class="edu_adv_box">
+                        <p class="edu_text">On trouve de nombreuses r&#232;gles de bon sens qui permettent de limiter l'exposition et par cons&#233;quent l'impact des ondes sur les personnes. Dans le cas du t&#233;l&#233;phone portable, nous pr&#233;conisons les mesures suivantes propos&#233;es par l'appel de 20 experts internationaux rassembl&#233;s par David Servan-Schreiber en 2008 <a class="link-to-pdf" href="">Link to PDF</a> Cet appel, fait echo &#224; l'appel des medecins de Fribourg lanc&#233; d&#232;s 2002. <a class="link-to-pdf" href="">Link to PDF</a> </p>
+                        <div class="more_info1">
+                            <a class="info_more1" href="">Lire</a>
+                            <img class="info_more_img1" src="Images/read_more.png" width="10px" alt="Read More" />
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
 
         </div>
-        
-    </div>
 
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="js/script.js"></script>     
+        <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>     
 
-<?php include("footer.php"); ?>   
-</body>
+        <?php include("footer.php"); ?>   
+    </body>
 </html>
